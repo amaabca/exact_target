e4287713e9d67fd32cf6800bf1599c4f8fd17f34
Michael van den Beuken <michael.beuken@gmail.com>
2016-05-07 13:44:44 -0600

    Make this compatible with more recent Rails versions that support more advanced functionality.

--- lib/exact_target/subscriber.rb
+++ lib/exact_target/subscriber.rb
@@ -1,6 +1,7 @@
 module ExactTarget
   class Subscriber
     include ActiveModel::Model
+    include ActiveRecord::AttributeAssignment if defined?(ActiveRecord::AttributeAssignment)
 
     attr_accessor :email, :travel_weekly, :enews, :travel_especials, :ins_enews, :deals_discounts, :fleet_contact,
                   :new_member_series, :personal_vehicle_reminder, :business_vehicle_reminder, :associate_vehicle_reminder,
